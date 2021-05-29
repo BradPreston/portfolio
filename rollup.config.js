86adb2e056a37f80d2a323d64fbce2d84a347f21
Bradley Preston <bradleypreston@Bradleys-MacBook-Pro.local>
2021-05-29 14:55:55 -0400

    fiex rollup config css output

--- rollup.config.js
+++ rollup.config.js
@@ -46,12 +46,12 @@ export default {
 			preprocess: autoPreprocess(),
 
 			css: css => {
-				css.write('public/bundle.css')
+				css.write('public/bundle.css');
 			}
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
